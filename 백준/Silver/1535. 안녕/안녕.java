7c4a137043d2138d81fc71d340d41153d068608e
Sangyoon <qlrqod123123@naver.com>
2025-05-23 17:04:23 +0900

    [Silver II] Title: 안녕, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int health = 100;
+        int[] L = new int[N + 1];
+        int[] J = new int[N + 1];
+        int[][] dp = new int[N + 1][health + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) L[i] = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) J[i] = Integer.parseInt(st.nextToken());
+
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= health; j++) {
+                if (L[i] >= j) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - L[i]] + J[i]);
+            }
+        }
+
+        System.out.print(dp[N][health]);
+    }
+}

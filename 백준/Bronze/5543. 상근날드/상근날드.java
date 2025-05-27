5a674e6e0bab35c31b5b5c8149f69f6501aef44d
Sangyoon <qlrqod123123@naver.com>
2025-05-27 11:45:00 +0900

    [Bronze IV] Title: 상근날드, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int burgerMin = Integer.MAX_VALUE;
+        int drinkMin = Integer.MAX_VALUE;
+
+        for (int i = 0; i < 3; i++) burgerMin = Math.min(burgerMin, Integer.parseInt(br.readLine()));
+        for (int i = 0; i < 2; i++) drinkMin = Math.min(drinkMin, Integer.parseInt(br.readLine()));
+
+        System.out.print(burgerMin + drinkMin - 50);
+    }
+}

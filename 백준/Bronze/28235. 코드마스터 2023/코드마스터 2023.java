793a21f74f4b621776a58a3f5602a52036fd6e53
Sangyoon <qlrqod123123@naver.com>
2025-06-07 23:35:27 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        switch (str) {
+            case "SONGDO":
+                System.out.print("HIGHSCHOOL");
+                break;
+            case "CODE":
+                System.out.print("MASTER");
+                break;
+            case "2023":
+                System.out.print("0611");
+                break;
+            case "ALGORITHM":
+                System.out.println("CONTEST");
+                break;
+        }
+    }
+}

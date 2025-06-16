35f59ca03cf0e872d9c7882e921c0fb32aec5745
Sangyoon <qlrqod123123@naver.com>
2025-06-16 10:09:36 +0900

    [Bronze I] Title: 캠핑, Time: 124 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            if (L == 0 && P == 0 && V == 0) break;
+
+            int result = L * (V / P) + (Math.min(V % P, L));
+            sb.append("Case ").append(count).append(": ").append(result).append("\n");
+            count++;
+        }
+
+        System.out.print(sb);
+    }
+}

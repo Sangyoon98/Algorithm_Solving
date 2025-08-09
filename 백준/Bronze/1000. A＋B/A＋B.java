9e9670e6e5c75ed03b986cd2571728f0bdc1f54f
Sangyoon <qlrqod123123@naver.com>
2025-08-09 22:36:23 +0900

    [Bronze V] Title: A+B, Time: 184 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		
-		System.out.println(a + b);
-	}
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int a, b;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    System.out.println(a + b);
+  }
 }

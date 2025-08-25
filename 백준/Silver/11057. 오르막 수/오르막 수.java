d178a7163619e35daa0c03b8a27cf68d9a7a87eb
Sangyoon <qlrqod123123@naver.com>
2025-08-25 12:07:13 +0900

    [Silver I] Title: 오르막 수, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][10];
+
+        for (int i = 0; i < 10; i++) dp[1][i] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i - 1][0];
+            for (int j = 1; j < 10; j++) {
+                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 10007;
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 10; i++) answer = (answer + dp[N][i]) % 10007;
+        System.out.print(answer);
+    }
+}

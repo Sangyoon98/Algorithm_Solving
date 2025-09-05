be02adade7b99dede98bb028f666f4d17467014b
Sangyoon <qlrqod123123@naver.com>
2025-09-06 01:05:07 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 512 ms, Memory: 50656 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        parent = new int[V + 1];
+        for (int i = 1; i <= V; i++) parent[i] = -1;
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            queue.add(new int[] {A, B, C});
+        }
+
+        long answer = 0;
+        int index = 0;
+        while (!queue.isEmpty() && index < V - 1) {
+            int[] curr = queue.poll();
+            if (find(curr[0]) != find(curr[1])) {
+                union(curr[0], curr[1]);
+                answer += curr[2];
+            }
+        }
+
+        System.out.print(answer);
+    }
+
+    static int find(int x) {
+        if (parent[x] < 0) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) parent[b] = a;
+    }
+}

12c97b44500d898cb45ac778eff6f7bd253147b0
Sangyoon <qlrqod123123@naver.com>
2025-11-21 21:59:51 +0900

    Title: 입력받아 계산, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251121/입력받아 계산/input-calculate.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+        bw.write(a + 2);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

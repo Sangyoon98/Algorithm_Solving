e60617752677c39564c9c895fd16260d3df89796
Sangyoon <qlrqod123123@naver.com>
2025-11-23 14:10:41 +0900

    [Gold II] Title: 저울, Time: 116 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        long answer = 1;
+
+        for (int x : arr) {
+            if (x > answer) break;
+            answer += x;
+        }
+
+        System.out.println(answer);
+    }
+}

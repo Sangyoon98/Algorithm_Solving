73174711a6ab6ed7519dabf83e36e3bbb7a6329c
Sangyoon <qlrqod123123@naver.com>
2025-12-01 15:28:34 +0900

    [Silver II] Title: 결혼식, Time: 160 ms, Memory: 18544 KB -BaekjoonHub

---
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        boolean[] visited = new boolean[n + 1];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] { 1, 0 });
+        visited[1] = true;
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int node = now[0];
+            int depth = now[1];
+            
+            if (depth >= 2) continue;
+            
+            for (int next : graph.get(node)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.add(new int[] { next, depth + 1 });
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}

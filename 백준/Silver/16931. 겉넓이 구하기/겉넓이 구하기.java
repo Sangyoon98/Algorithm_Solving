2ee45302305ff9dc0dc2988b69200fc8cb77f277
Sangyoon <qlrqod123123@naver.com>
2025-12-28 14:42:35 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 136 ms, Memory: 15344 KB -BaekjoonHub

---
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
+        int answer = N * M * 2;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                for (int d = 0; d < 4; d++) {
+                    int nx = i + dx[d];
+                    int ny = j + dy[d];
+
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                        answer += map[i][j];
+                    } else {
+                        answer += Math.max(0, map[i][j] - map[nx][ny]);
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}

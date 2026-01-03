facf694fd8fc7238e2a10973967a7f758283c6d0
Sangyoon <qlrqod123123@naver.com>
2026-01-03 13:09:38 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int d = Integer.parseInt(st.nextToken());
+            min = Math.min(min, d);
+            max = Math.max(max, d);
+        }
+
+        System.out.println(min * max);
+    }
+}

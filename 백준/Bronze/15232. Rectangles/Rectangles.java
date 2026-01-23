effa4f34ef5724d54055d6e855b42a119e9a98d7
Sangyoon <qlrqod123123@naver.com>
2026-01-23 21:43:39 +0900

    [Bronze V] Title: Rectangles, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int R = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < R; i++) {
+            sb.append("*".repeat(C)).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
